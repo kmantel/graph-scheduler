7b7f6d5246666e895d8b846f644b9cf799247d53
Katherine Mantel <kmantel@princeton.edu>
2021-07-13 20:37:52 -0400

    maint: remove unneeded psyneulink code

--- conftest.py
+++ conftest.py
@@ -1,48 +1,13 @@
 import doctest
-import psyneulink
 import pytest
-import numpy as np
-
 
 from psyneulink import clear_registry, primary_registries
 from psyneulink.core import llvm as pnlvm
-from psyneulink.core.globals.utilities import set_global_seed
 
-
-try:
-    import torch
-    pytorch_available = True
-except ImportError:
-    pytorch_available = False
-
-# def pytest_addoption(parser):
-#     parser.addoption(
-#         '--pnl-seed',
-#         action='store',
-#         default=int(time.time() * 256),
-#         help='the seed to use for each test'
-#     )
-
-mark_stress_tests = 'stress'
-
-marks_default_skip = [mark_stress_tests]
-
-
-# skip stress tests by default, add command option to include
-# http://blog.devork.be/2009/12/skipping-slow-test-by-default-in-pytest.html
-def pytest_addoption(parser):
-    parser.addoption('--{0}'.format(mark_stress_tests), action='store_true', default=False, help='Run {0} tests (long)'.format(mark_stress_tests))
 
 def pytest_runtest_setup(item):
-    for m in marks_default_skip:
-        if m in item.keywords and not item.config.getvalue(m):
-            pytest.skip('{0} tests not requested'.format(m))
-
     if 'cuda' in item.keywords and not pnlvm.ptx_enabled:
             pytest.skip('PTX engine not enabled/available')
-
-    if 'pytorch' in item.keywords and not pytorch_available:
-            pytest.skip('pytorch not available')
 
     doctest.ELLIPSIS_MARKER = "[...]"
 
@@ -72,16 +37,6 @@ def pytest_generate_tests(metafunc):
                       pytest.param(pnlvm.ExecutionMode.LLVMRun, marks=pytest.mark.llvm)]
         metafunc.parametrize("autodiff_mode", auto_modes)
 
-def pytest_runtest_call(item):
-    # seed = int(item.config.getoption('--pnl-seed'))
-    seed = 0
-    np.random.seed(seed)
-    set_global_seed(seed)
-
-    if 'pytorch' in item.keywords:
-        assert pytorch_available
-        torch.manual_seed(seed)
-
 
 def pytest_runtest_teardown(item):
     for registry in primary_registries:
@@ -104,62 +59,3 @@ def get_comp_execution_modes():
             pytest.param(pnlvm.ExecutionMode.PTXExec, marks=[pytest.mark.llvm, pytest.mark.cuda]),
             pytest.param(pnlvm.ExecutionMode.PTXRun, marks=[pytest.mark.llvm,  pytest.mark.cuda])
            ]
-
-@pytest.helpers.register
-def cuda_param(val):
-    return pytest.param(val, marks=[pytest.mark.llvm, pytest.mark.cuda])
-
-@pytest.helpers.register
-def get_func_execution(func, func_mode):
-    if func_mode == 'LLVM':
-        return pnlvm.execution.FuncExecution(func).execute
-    elif func_mode == 'PTX':
-        return pnlvm.execution.FuncExecution(func).cuda_execute
-    elif func_mode == 'Python':
-        return func.function
-    else:
-        assert False, "Unknown function mode: {}".format(mode)
-
-@pytest.helpers.register
-def get_mech_execution(mech, mech_mode):
-    if mech_mode == 'LLVM':
-        return pnlvm.execution.MechExecution(mech).execute
-    elif mech_mode == 'PTX':
-        return pnlvm.execution.MechExecution(mech).cuda_execute
-    elif mech_mode == 'Python':
-        def mech_wrapper(x):
-            mech.execute(x)
-            return mech.output_values
-        return mech_wrapper
-    else:
-        assert False, "Unknown mechanism mode: {}".format(mode)
-
-@pytest.helpers.register
-def expand_np_ndarray(arr):
-    # this will fail on an input containing a float (not np.ndarray)
-    try:
-        iter(arr)
-    except TypeError:
-        return arr.tolist()
-
-    results_list = []
-    for elem in arr:
-        try:
-            iter(elem)
-        except TypeError:
-            elem = [elem]
-
-        for nested_elem in elem:
-            nested_elem = nested_elem.tolist()
-            try:
-                iter(nested_elem)
-            except TypeError:
-                nested_elem = [nested_elem]
-            results_list.extend(nested_elem)
-    return results_list
-
-
-# flag when run from pytest
-# https://docs.pytest.org/en/stable/example/simple.html#detect-if-running-from-within-a-pytest-run
-def pytest_configure(config):
-    psyneulink._called_from_pytest = True
