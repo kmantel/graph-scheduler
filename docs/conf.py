613ac3437bd571bd93c2a55e1cc23f172d450ba9
Katherine Mantel <kmantel@princeton.edu>
2021-07-13 21:14:18 -0400

    docs: add intersphinx mappings

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ version = '.'.join(release.split('.')[:3])
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx_rtd_theme',
 ]
@@ -65,6 +66,11 @@ html_static_path = ['_static']
 
 autodoc_member_order = 'bysource'
 default_role = 'any'
+
+intersphinx_mapping = {
+    'networkx': ('https://networkx.org/documentation/stable/', None),
+    'pint': ('https://pint.readthedocs.io/en/stable/', None),
+}
 
 
 def setup(app):
